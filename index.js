b4e98598e7f3071b32a64d77378cb9537bd7108c
Gaurav36Mishra <gaurav05mishra@gmail.com>
2018-08-13 15:52:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ restService.post("/echo", function(req, res) {
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    speech: "Thank you for talking to HDFC Life. Our agent will talk to you soon",
-    displayText: "Thank you for talking to HDFC Life. Our agent will talk to you soon",
+    speech: "Thank you for talking to HDFC Life. Our agent will contact you soon",
+    displayText: "Thank you for talking to HDFC Life. Our agent will contact you soon",
     source: "HDFC-Life-Bot"
   });
 });
